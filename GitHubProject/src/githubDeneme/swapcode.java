4697dcc691a07a621a7c822e8a365fb8c7dfe404
thehilalss <113767181+thehilalss@users.noreply.github.com>
2022-10-24 19:33:00 +0300

    swap.code2

---
+++ GitHubProject/src/githubDeneme/swapcode.java
@@ -0,0 +1,31 @@
+package githubDeneme;
+
+import java.util.Scanner;
+
+public class swapcode {
+	
+	 public static void main(String[] args) {
+	    	Scanner scan = new Scanner(System.in);
+					
+
+						
+	    	int[] array = {12,23,67,56,78,89,45,34,29,30};
+	    	swap(array);
+	    	for(int i: array) {
+	    		System.out.printf(i + " ");
+		}
+	    }		
+	    
+		public static void swap(int[] array) {
+			for(int firstEl=0, lastEl=array.length-1;firstEl<=4;firstEl++,lastEl--) {
+				int temp = array[lastEl];
+				array[lastEl]=array[firstEl];
+				array[firstEl]=temp;
+		}				
+		}
+	}
+
+
+
+
+
